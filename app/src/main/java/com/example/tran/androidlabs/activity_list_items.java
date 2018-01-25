8aeb4d021889b3d7bd9d09c5decc64f708dee01c
Tran <le000148@algonquinlive.com>
2018-01-24 22:51:29 -0500

    added 3 activities for lab2

---
+++ app/src/main/java/com/example/tran/androidlabs/activity_list_items.java
@@ -0,0 +1,13 @@
+package com.example.tran.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_list_items extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
